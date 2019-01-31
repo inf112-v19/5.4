d25b2f47e580a4078c0cf1b8bf42fb8e56c82bdf
Øyvind Straume Almli <oyvind.almli@gmail.com>
2019-01-31 17:54:33 +0100

    Added package board, Added ICell to board

---
+++ src/main/java/inf112/skeleton/app/board/ICell.java
@@ -0,0 +1,16 @@
+package inf112.skeleton.app.board;
+
+public interface ICell {
+    /**
+     * Returns the type of cell
+     * @return ICell type of cell
+     */
+    ICell getType();
+
+    /**
+     * Returns the name of the Cell
+     * @return String name of Cell
+     */
+    String getName();
+
+}
