d617537d50f0922ddb04b0e31a92c5b4ce07fa09
Øyvind Straume Almli <oyvind.almli@gmail.com>
2019-01-31 12:42:43 +0100

    Swapped from int rotationDelta to Enum Rotation

--- src/main/java/inf112/skeleton/app/Enum/CardType.java
+++ src/main/java/inf112/skeleton/app/Enum/CardType.java
@@ -1,22 +1,22 @@
 package inf112.skeleton.app.Enum;
 
 public enum CardType {
-    ROTATE_LEFT("Turn Left", 0, -90),
-    ROTATE_RIGHT("Turn Right", 0, 90),
-    ROTATE_U("U-Turn", 0, 180),
-    MOVE_1("Move 1 Forward", 1, 0),
-    MOVE_2("Move 2 Forward", 2, 0),
-    MOVE_3("Move 3 Forward", 3, 0),
-    BACK_UP("Move 1 Backwards", -1, 0);
+    ROTATE_LEFT("Turn Left", 0, Rotation.L),
+    ROTATE_RIGHT("Turn Right", 0, Rotation.R),
+    ROTATE_U("U-Turn", 0, Rotation.U),
+    MOVE_1("Move 1 Forward", 1, null),
+    MOVE_2("Move 2 Forward", 2, null),
+    MOVE_3("Move 3 Forward", 3, null),
+    BACK_UP("Move 1 Backwards", -1, null);
 
     private String description;
     private int moveDelta;
-    private int rotationDelta;
+    private Rotation rotation;
 
-    CardType(String description, int moveDelta, int rotationDelta){
+    CardType(String description, int moveDelta, Rotation rotation){
         this.description = description;
         this.moveDelta = moveDelta;
-        this.rotationDelta = rotationDelta;
+        this.rotation = rotation;
     }
 
     public String getDescription(){
@@ -27,7 +27,7 @@ public enum CardType {
         return moveDelta;
     }
 
-    public int getRotationDelta() {
-        return rotationDelta;
+    public Rotation getRotationDelta() {
+        return rotation;
     }
 }
