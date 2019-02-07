69a2fb6ae687ed007208ad2f68d4ac2e42a7bfcb
Zylvian <jovlisen@gmail.com>
2019-02-07 03:16:04 +0100

    Fixed JSONGenerator path
    Use local paths from assets from now on.
    
    Gdx.files.internal("file");

--- src/main/java/inf112/skeleton/app/Game/board/JSONBoardGenerator.java
+++ src/main/java/inf112/skeleton/app/Game/board/JSONBoardGenerator.java
@@ -1,9 +1,12 @@
 package inf112.skeleton.app.Game.board;
 
+import inf112.skeleton.app.Game.Enum.Direction;
+import inf112.skeleton.app.Game.board.pieces.Wall;
 import org.json.simple.JSONArray;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -16,7 +19,7 @@ public class JSONBoardGenerator {
     public void generateJsonBoard() {
 
         try {
-            Object boardFile = parser.parse(new FileReader("C:\\Users\\motre\\IdeaProjects\\jsonParseTest\\JsonParseTest\\src\\main\\java\\JSONBoard.json"));
+            Object boardFile = parser.parse(new FileReader("C:\\Users\\Morten\\IdeaProjects\\jsonParseTest\\jsonParseTesto\\src\\main\\java\\PackageMcGoo\\TestBoard.json"));
             JSONObject jsonBoardFile = (JSONObject) boardFile;
             System.out.println(jsonBoardFile);
             for (int x = 0; x <= 9; x++) {
@@ -26,8 +29,51 @@ public class JSONBoardGenerator {
 
                     JSONObject xCord = (JSONObject) jsonBoardFile.get(intX);
                     JSONArray yCord = (JSONArray) xCord.get(intY);
+                    Iterator<String> iterator = yCord.iterator();
+                    while (iterator.hasNext()) {
+                        String temp = iterator.next();
+                        System.out.println(temp);
+                        switch (temp) {
+                            case "Hole":
+                                System.out.println("Making Hole!");
+                                break;
+                            case "NorthWall":
+                                System.out.println("Making northfacing wall!");
+                                new Wall(Direction.NORTH);
+                                break;
+                            case "EastWall":
+                                System.out.println("Making eastfacing wall!");
+                                new Wall(Direction.EAST);
+                                break;
+                            case "SouthWall":
+                                System.out.println("Making southfacing wall!");
+                                new Wall(Direction.SOUTH);
+                                break;
+                            case "WestWall":
+                                System.out.println("Making westfacing wall!");
+                                new Wall(Direction.NORTH);
+                                break;
+                            case "ConveyorNorth":
+                                System.out.println("Making northfacing conveyor!");
+                                new Wall(Direction.NORTH);
+                                break;
+                            case "ConveyorEast":
+                                System.out.println("Making eastfacing conveyor!");
+                                new Wall(Direction.NORTH);
+                                break;
+                            case "ConveyorSouth":
+                                System.out.println("Making southfacing conveyor!");
+                                new Wall(Direction.NORTH);
+                                break;
+                            case "ConveyorWest":
+                                System.out.println("Making westfacing conveyor!");
+                                new Wall(Direction.NORTH);
+                                break;
 
-                    System.out.println(yCord);
+                        }
+
+                    }
+                    //System.out.println(yCord);
                 }
             }
         } catch (FileNotFoundException e) {
