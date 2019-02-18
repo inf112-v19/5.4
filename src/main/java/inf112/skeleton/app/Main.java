1ceea7b397a8e93470df1b0259f60c8bab82ba1e
Motrendo <motrendo@gmail.com>
2019-02-18 13:02:54 +0100

    Import fix in main

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import inf112.skeleton.app.GUI.Scene2DGangBangChang;
-import inf112.skeleton.app.Game.board.Board;
 
 
 public class Main {
