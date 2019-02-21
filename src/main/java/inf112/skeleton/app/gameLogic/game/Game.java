35523469822c57b7c201da04a9fd0479a546507f
Marie Heggebakk <marie.heggebakk@gmail.com>
2019-02-21 21:31:02 +0100

    Created the methods for the three phases in the game.

--- src/main/java/inf112/skeleton/app/gameLogic/game/Game.java
+++ src/main/java/inf112/skeleton/app/gameLogic/game/Game.java
@@ -4,13 +4,28 @@ public class Game {
 
     public void playGame(){
 
-        doPhase1();
+        prePlay();
+        play();
+        postPlay();
+    }
 
-
+    /**
+     * First phase in the game
+     * Here the player will get to draw and pick cards
+     */
+    private void prePlay() {
 
     }
 
-    public void doPhase1(){
+    /**
+     * Second phase in the game
+     * Here the game will execute the cards the player picked, check death, flags and conveyor
+     */
+    private void play() {
+
+    }
+
+    private void postPlay() {
 
     }
 }
