8c5dad8545636747501526a7092b975b8dc3d127
Marie Heggebakk <marie.heggebakk@gmail.com>
2019-02-21 21:56:44 +0100

    Calculated number of cards the player is going to draw

--- src/main/java/inf112/skeleton/app/gameLogic/game/Game.java
+++ src/main/java/inf112/skeleton/app/gameLogic/game/Game.java
@@ -1,10 +1,12 @@
 package inf112.skeleton.app.gameLogic.game;
 
+import inf112.skeleton.app.gameLogic.Player;
+import inf112.skeleton.app.gameLogic.ProgramCardDeck;
+
 public class Game {
 
-    public void playGame(){
-
-        prePlay();
+    public void playGame(Player currentPlayer, ProgramCardDeck programCards){
+        prePlay(currentPlayer);
         play();
         postPlay();
     }
@@ -13,7 +15,17 @@ public class Game {
      * First phase in the game
      * Here the player will get to draw and pick cards
      */
-    private void prePlay() {
+    private void prePlay(Player currentPlayer) {
+        int damageTokens = currentPlayer.getDamageTokens();
+        int cardsToDraw = 9;
+        cardsToDraw -= damageTokens;
+
+        for (int i = 0; i < cardsToDraw; i++) {
+            // TODO take cards from deck and assign them to the player
+        }
+
+
+
 
     }
 
