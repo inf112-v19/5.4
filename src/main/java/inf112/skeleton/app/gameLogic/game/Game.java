2e882ef1067e6832c07a8c0bfd5022634b76ddb0
Marie Heggebakk <marie.heggebakk@gmail.com>
2019-02-22 15:51:44 +0100

    Started on setting up the game.

--- src/main/java/inf112/skeleton/app/gameLogic/game/Game.java
+++ src/main/java/inf112/skeleton/app/gameLogic/game/Game.java
@@ -1,9 +1,21 @@
 package inf112.skeleton.app.gameLogic.game;
 
+import inf112.skeleton.app.GUI.player.Position;
 import inf112.skeleton.app.gameLogic.Player;
 import inf112.skeleton.app.gameLogic.ProgramCardDeck;
+import inf112.skeleton.app.gameLogic.enums.Direction;
 
 public class Game {
+    private int totalPlayers = 2;   // Total players in the game
+    private Player[] players;       // Players in the game
+
+    public void setupGame() {
+        players = new Player[totalPlayers];
+        for (int i = 0; i < players.length; i++) {
+            Position position = new Position(i, 0);
+            players[i] = new Player(position, Direction.NORTH, 10);
+        }
+    }
 
     public void playGame(Player currentPlayer, ProgramCardDeck programCards){
         prePlay(currentPlayer);
