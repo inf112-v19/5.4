02298f2a881054ba14006eb39ac50ac70a65b54d
Marie Heggebakk <marie.heggebakk@gmail.com>
2019-03-04 18:12:14 +0100

    First phase is now partly working. Started on phase two.

--- src/test/java/inf112/skeleton/app/RoboRallyGameTest.java
+++ src/test/java/inf112/skeleton/app/RoboRallyGameTest.java
@@ -1,5 +1,6 @@
 package inf112.skeleton.app;
 
+import inf112.skeleton.app.GUI.MainGameScreen;
 import inf112.skeleton.app.GUI.player.Position;
 import inf112.skeleton.app.gameLogic.Player;
 import inf112.skeleton.app.gameLogic.game.RoboRallyGame;
@@ -10,12 +11,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RoboRallyGameTest {
     private RoboRallyGame game;
+    private MainGameScreen gameScreen;
 
     @BeforeEach
     void setup() {
-        this.game = new RoboRallyGame();
+        this.game = new RoboRallyGame(gameScreen);
     }
-
 
     @Test
     void checkSetup() {
@@ -23,50 +24,5 @@ class RoboRallyGameTest {
         assertEquals(new Position(0, 0), game.players[0].getPos());
         assertEquals(new Position(1,0), game.players[1].getPos());
         assertEquals(84, game.deck.numCardsLeftInDeck());
-    }
-
-    @Test
-    void chooseCardsTest() {
-        Player currentPlayer = game.players[0];
-        assertEquals(0, currentPlayer.getPlayerDeckSize());
-
-        game.chooseCards(currentPlayer, 9);
-        assertEquals(9, currentPlayer.getPlayerDeckSize());
-    }
-
-    @Test
-    void playerPickCardsTest() {
-        Player currentPlayer = game.players[1];
-        game.playerPickCards(currentPlayer);
-        game.chooseCards(currentPlayer, 9);
-        game.playerPickCards(currentPlayer);
-    }
-
-    @Test
-    void getCardsForEachRegister() {
-
-//        game.play();
-    }
-    @Test
-    void chooseCardsTest() {
-        Player currentPlayer = game.players[0];
-        assertEquals(0, currentPlayer.getPlayerDeckSize());
-
-        game.chooseCards(currentPlayer, 9);
-        assertEquals(9, currentPlayer.getPlayerDeckSize());
-    }
-
-    @Test
-    void playerPickCardsTest() {
-        Player currentPlayer = game.players[1];
-        game.playerPickCards(currentPlayer);
-        game.chooseCards(currentPlayer, 9);
-        game.playerPickCards(currentPlayer);
-    }
-
-    @Test
-    void getCardsForEachRegister() {
-
-//        game.play();
     }
 }
