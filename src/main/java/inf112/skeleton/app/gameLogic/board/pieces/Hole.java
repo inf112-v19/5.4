1a3bf7a2edd1a81f414e9285eee8f4d56f04a7de
Motrendo <motrendo@gmail.com>
2019-03-06 13:23:57 +0100

    Expanded the switch case in the JSONBoardGenerator and added Hole as a piece.

---
+++ src/main/java/inf112/skeleton/app/gameLogic/board/pieces/Hole.java
@@ -0,0 +1,31 @@
+package inf112.skeleton.app.gameLogic.board.pieces;
+
+import inf112.skeleton.app.gameLogic.board.IPiece;
+import inf112.skeleton.app.gameLogic.enums.Direction;
+
+public class Hole implements IPiece {
+
+    public Hole () {
+
+    }
+
+    @Override
+    public IPiece getType() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public char getSymbol() {
+        return 0;
+    }
+
+    @Override
+    public Direction getRotation() {
+        return null;
+    }
+}
