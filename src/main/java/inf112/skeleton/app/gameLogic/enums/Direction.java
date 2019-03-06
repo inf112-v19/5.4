ce869ad98a995d8941deec5c07e11883f85c9d5b
Øyvind Straume Almli <oyvind.almli@gmail.com>
2019-03-06 13:53:13 +0100

    Added oppositeDirection to Direction IPlayer now Extends IPiece

--- src/main/java/inf112/skeleton/app/gameLogic/enums/Direction.java
+++ src/main/java/inf112/skeleton/app/gameLogic/enums/Direction.java
@@ -1,5 +1,18 @@
 package inf112.skeleton.app.gameLogic.enums;
 
 public enum Direction {
-    NORTH, SOUTH, EAST, WEST
+    NORTH, SOUTH, EAST, WEST;
+
+    public Direction oppositeDir(Direction dir) {
+        switch (dir) {
+            case NORTH:
+                return Direction.SOUTH;
+            case SOUTH:
+                return Direction.NORTH;
+            case EAST:
+                return Direction.WEST;
+            default:
+                return Direction.EAST;
+        }
+    }
 }
--- src/main/java/inf112/skeleton/app/gameLogic/game/Checker.java
+++ src/main/java/inf112/skeleton/app/gameLogic/game/Checker.java
@@ -10,8 +10,6 @@ import inf112.skeleton.app.gameLogic.enums.Direction;
 
 import java.util.List;
 
-import static inf112.skeleton.app.gameLogic.enums.ActionType.MOVE;
-
 public class Checker {
     private Position position;
     private Action playersAction;
@@ -23,19 +21,6 @@ public class Checker {
 
     }
 
-    private Direction oppositeDir(Direction dir) {
-        switch (dir) {
-            case NORTH:
-                return Direction.SOUTH;
-            case SOUTH:
-                return Direction.NORTH;
-            case EAST:
-                return Direction.WEST;
-            default:
-                return Direction.EAST;
-        }
-    }
-
     public boolean canMove(Direction goingDir, ICell currCell, ICell nextCell) {
         List<IPiece> piecesInCurrCell = currCell.getPiecesInCell();
         List<IPiece> piecesInNextCell = nextCell.getPiecesInCell();
@@ -44,9 +29,17 @@ public class Checker {
                 return false;
             }
         }
-        Direction oppositeDir = oppositeDir(goingDir);
+        Direction oppositeDir = goingDir.oppositeDir(goingDir);
         for (IPiece piece : piecesInNextCell) {
             if (piece instanceof Wall && piece.getRotation() == oppositeDir) {
+                if(piece instanceof Player){
+                    /*
+                    Torjus sin makeRecursiveCollision();
+                    For å styre spilleren
+                    Player player = (Player) piece;
+                    player.move();
+                    */
+                }
                 return false;
             }
         }
