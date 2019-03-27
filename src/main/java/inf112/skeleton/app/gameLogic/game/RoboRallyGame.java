f541873249a822104f661a61a17f97aacc64d1b4
Marie Heggebakk <marie.heggebakk@gmail.com>
2019-03-27 13:08:05 +0100

    Refactored RoboRallyGame

--- src/main/java/inf112/skeleton/app/gameLogic/game/RoboRallyGame.java
+++ src/main/java/inf112/skeleton/app/gameLogic/game/RoboRallyGame.java
@@ -67,6 +67,7 @@ public class RoboRallyGame {
         cardsToDraw -= damageTokens;
 
         List<ProgramCard> cards = deck.drawXCards(cardsToDraw);
+
         this.guiScreen.pickCardPhase(cards);
     }
 
