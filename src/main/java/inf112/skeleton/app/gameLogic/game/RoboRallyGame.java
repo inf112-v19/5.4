9de47ccfc14094e84b84af09104be4411ae55504
Marie Heggebakk <marie.heggebakk@gmail.com>
2019-03-27 13:08:48 +0100

    Added get methods

--- src/main/java/inf112/skeleton/app/gameLogic/game/RoboRallyGame.java
+++ src/main/java/inf112/skeleton/app/gameLogic/game/RoboRallyGame.java
@@ -95,4 +95,20 @@ public class RoboRallyGame {
     public Player[] getPlayers(){
         return this.players;
     }
+
+    public int getTotalPlayers() {
+        return this.totalPlayers;
+    }
+
+    public int getStartHealth() {
+        return this.startHealth;
+    }
+
+    public Player getCurrentPlayer() {
+        return this.currentPlayer;
+    }
+
+    public ProgramCardDeck getDeck() {
+        return this.deck;
+    }
 }
