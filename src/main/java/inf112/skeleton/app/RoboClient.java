472ea2484545ec4a9c53b14191e9aa68620d6121
Motrendo <motrendo@gmail.com>
2019-03-29 13:28:01 +0100

    Updated some dank server shit

--- src/main/java/inf112/skeleton/app/RoboClient.java
+++ src/main/java/inf112/skeleton/app/RoboClient.java
@@ -1,15 +1,32 @@
 package inf112.skeleton.app;
 
+import inf112.skeleton.app.gameLogic.Player;
+
+import java.io.*;
 import java.net.Socket;
 
 public class RoboClient {
 
-    public void tryConnection (String ip, int port) throws Exception {
+    private Player clientPlayer;
+    private Socket roboClient;
+    private DataInputStream in;
+    private DataOutputStream out;
 
-        try (Socket roboClient = new Socket(ip, port)) {
-
+    public RoboClient( String ip, int port){
+        try {
+            System.out.println("Attempting connection to " + ip + " on port " + port);
+            roboClient = new Socket(ip, port);
             System.out.println("Connection accepted! Do some shit.");
 
+            out = new DataOutputStream(roboClient.getOutputStream());
+            in = new DataInputStream(roboClient.getInputStream());
+
+            out.writeUTF("Schmell from client");
+            System.out.println("The server sends " + in.readUTF());
+            roboClient.close();
+        }
+        catch (IOException e) {
+            System.out.println(e);
         }
     }
 }
--- src/main/java/inf112/skeleton/app/RoboServer.java
+++ src/main/java/inf112/skeleton/app/RoboServer.java
@@ -1,20 +1,49 @@
 package inf112.skeleton.app;
 
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
-public class RoboServer {
+public class RoboServer extends Thread {
 
-    public static void main(String[] args) throws Exception {
+    private ServerSocket roboServer;
+    private DataInputStream in;
+    private DataOutputStream out;
 
-        int port = 8000;
-        ServerSocket roboServer = new ServerSocket(port);
-        System.out.println("Listening for connection on port " + port + "...");
+    public RoboServer (int port) throws IOException {
+        roboServer = new ServerSocket(port);
+        roboServer.setSoTimeout(100000);
+    }
+    public void runServer() {
+        while (true) {
+            try {
 
-            while (true) {
-                try (Socket roboClient = roboServer.accept()) {
-                    System.out.println("Connection request accepted");
-                }
+                System.out.println("Waiting for client");
+                Socket roboSocket = roboServer.accept();
+                System.out.println("Client connected");
+
+                in = new DataInputStream(roboSocket.getInputStream());
+                out = new DataOutputStream(roboSocket.getOutputStream());
+
+                System.out.println(in.readUTF());
+
+                out.writeUTF("This be from dat server fam.");
+
+                roboServer.close();
+
+            } catch (SocketTimeoutException s) {
+                System.out.println("Socket timed out.");
+                break;
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                break;
             }
         }
     }
+    public static void main (String[] args) throws IOException {
+        RoboServer roboServer = new RoboServer(8000);
+        roboServer.runServer();
+    }
+}
