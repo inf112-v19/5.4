5f37bbd0594dadf0d190b8610627935be5502b1a
Marie Heggebakk <marie.heggebakk@gmail.com>
2019-04-01 01:48:54 +0200

    Created tests for RoboRallyGame, sanity tests.

--- src/test/java/inf112/skeleton/app/GameLogicTest.java
+++ src/test/java/inf112/skeleton/app/GameLogicTest.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglFiles;
 import com.badlogic.gdx.backends.lwjgl.LwjglNativesLoader;
 import inf112.skeleton.app.GUI.GUIMain;
-import inf112.skeleton.app.GUI.MainGameScreen;
 import inf112.skeleton.app.GUI.player.Position;
 import inf112.skeleton.app.gameLogic.Player;
 import inf112.skeleton.app.gameLogic.board.Board;
@@ -13,8 +12,11 @@ import inf112.skeleton.app.gameLogic.enums.Direction;
 import inf112.skeleton.app.gameLogic.game.RoboRallyGame;
 import inf112.skeleton.app.gameLogic.game.TestGame;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameLogicTest extends GameTest {
 
@@ -22,7 +24,7 @@ public class GameLogicTest extends GameTest {
     private TestGame game;
     private Board board;
 
-    @Test
+    @BeforeEach
     public void setup() {
         LwjglNativesLoader.load();
         Gdx.files = new LwjglFiles();
@@ -33,7 +35,17 @@ public class GameLogicTest extends GameTest {
         game = new TestGame();
         player = new Player(new Position(7, 7), Direction.NORTH, 3, game.getBoard());
 
+    }
+    @Test
+    public void sanityTest() {
         RoboRallyGame rrgame = new RoboRallyGame();
+        assertEquals(3, rrgame.getTotalPlayers());
+        assertEquals(3, rrgame.getStartHealth());
     }
 
+    @Test
+    public void test() {
+        RoboRallyGame rrgame = new RoboRallyGame();
+
+    }
 }
