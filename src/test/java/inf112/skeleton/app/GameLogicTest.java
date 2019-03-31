2c29419265ce75d2e69e5f9b2e76ba1909b332a9
Zylvian <jovlisen@gmail.com>
2019-04-01 00:00:26 +0200

    Started on GameLogicTest

--- src/test/java/inf112/skeleton/app/GameLogicTest.java
+++ src/test/java/inf112/skeleton/app/GameLogicTest.java
@@ -1,15 +1,41 @@
 package inf112.skeleton.app;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglFiles;
+import com.badlogic.gdx.backends.lwjgl.LwjglNativesLoader;
+import inf112.skeleton.app.GUI.GUIMain;
 import inf112.skeleton.app.GUI.MainGameScreen;
+import inf112.skeleton.app.GUI.player.Position;
+import inf112.skeleton.app.gameLogic.Player;
+import inf112.skeleton.app.gameLogic.board.Board;
+import inf112.skeleton.app.gameLogic.enums.Direction;
 import inf112.skeleton.app.gameLogic.game.RoboRallyGame;
+import inf112.skeleton.app.gameLogic.game.TestGame;
 import org.junit.Test;
+
+import java.io.File;
 
 public class GameLogicTest extends GameTest {
 
+    private Player player;
+    private TestGame game;
+    private Board board;
+
     @Test
     public void setup() {
+        LwjglNativesLoader.load();
+        Gdx.files = new LwjglFiles();
+
+        System.out.println(new File("DankBoard.json").getAbsoluteFile());
+        new LwjglApplication(new GUIMain());
+        this.board = new Board("Captain Hook", "DankBoard.json");
+        game = new TestGame();
+        player = new Player(new Position(7, 7), Direction.NORTH, 3, game.getBoard());
+
+
         MainGameScreen mgs = new MainGameScreen();
         RoboRallyGame game = new RoboRallyGame(mgs);
-
     }
+
 }
